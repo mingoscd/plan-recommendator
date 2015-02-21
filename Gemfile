32eef0539435872fbb9920970da851b5e8bc59e4
Mingos <dcividanes91@gmail.com>
2015-02-21 03:14:12 +0100

    finished lightbox and user avatar

--- Gemfile
+++ Gemfile
@@ -7,11 +7,8 @@ gem 'rails', '4.2.0'
 gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
-gem 'bootstrap-sass'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-# Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
@@ -23,7 +20,6 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/hleon-js.js
+++ app/assets/javascripts/hleon-js.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 	// Button Burguer animation
 	$("#js-btn--burguer").click(function() {
-	  	$(this).toggleClass("js-active");
+	  $(this).toggleClass("js-active");
 		toggleMenu();
 		toggleShadow();
 		toggleBlockScroll();
@@ -12,12 +12,36 @@ $(document).ready(function(){
 	function toggleMenu(){
 		$("#js-toggle-menu").toggleClass("js-toogle-menu");
 	};
-	function toggleShadow(){
-		$("#js-lightbox").toggleClass("js-lightbox--active");
-	}
 	function toggleBlockScroll(){
 		$("body").toggleClass("js-block-scroll");
 	}
+
+
+	/* -----------------------------
+	/*					LIGHTBOX
+	/* -----------------------------
+	/*
+	/* The lightbox is used to put a shadow that puts in background some content. This feature has
+	/* the option to add a class to the parent element to put over the lightbox and stay in foreground.
+	/* Example to Toggle Lightbox Effect: <button onclick="toggleShadow()">Click me </button>
+	/* Example element over lightbox: <nav class="over-lightbox">...</nav>
+	/*
+	/* ----------------------------- */ 
+
+	function toggleShadow(){
+		$("#js-lightbox").length ? removeLightbox() : createLightbox();
+	}
+	function createLightbox(){
+		$(".over-lightbox").addClass("js-over-lightbox--active");
+		$("body").append('<div id="js-lightbox" class="lightbox"></div>');
+		$("#js-lightbox").addClass("js-lightbox--active");
+	}
+	function removeLightbox(){
+		$("#js-lightbox").removeClass("js-lightbox--active");
+		setTimeout(function(){ $(".over-lightbox").removeClass("js-over-lightbox--active")}, 500);
+		$("#js-lightbox").remove();
+	}
+
 
 
 
