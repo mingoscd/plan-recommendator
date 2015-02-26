928f732814af8488415721fc034e6f40d31a484e
Mingos <dcividanes91@gmail.com>
2015-02-26 18:22:53 +0100

    suggest 2nd iteration

--- app/controllers/suggests_controller.rb
+++ app/controllers/suggests_controller.rb
@@ -10,20 +10,39 @@ class SuggestsController < ApplicationController
 
 		#generar punto de partida (cercano)
 		#init parameters
-		@plan = []
-		i = 0 
-		step = 0 
+		@plan, i , step = [], 0 , 0
+		@cultural_types = ["Monument", "Museum", "Other"]
+		@relax_types = ["Park", "Cafe", "Beautiful Place", "Beach"]
+		@drink_types = ["Bar", "Disco", "Party"]
+
 		while i == 0  do
-   		step += 0.01
+   		step += 0.008
    		range_lat = (point_lat - step) .. (point_lat + step)
    		range_lon = (point_lon - step) .. (point_lon + step)
    		point = generate_begin_point(range_lat,range_lon)
    		i = point.count
 		end
 		@plan << point.sample
-
-		#otros puntos de interes cercanos en bucle (4-5)
-
+		#otros puntos de interes cercanos en bucle (4-5) #if time is filled could be an option
+		3.times do 
+			point_lat = @plan.last.lat.to_f
+			point_lon = @plan.last.lon.to_f
+			step , i = 0 , 0
+			while i == 0  do
+	   		step += 0.002
+	   		range_lat = (point_lat - step) .. (point_lat + step)
+	   		range_lon = (point_lon - step) .. (point_lon + step)
+	   		point = generate_random_point(range_lat,range_lon)
+	   		#check no repeated places
+	   		if point.count > 0
+		   		place = point.sample
+		   		unless @plan.collect { |p| p.name }.include? place.name 
+		   			i = point.count
+		   		end
+		   	end
+			end
+		@plan << place
+		end
 		#incluir lugar de descanso
 
 		#mooore
@@ -45,16 +64,44 @@ class SuggestsController < ApplicationController
 		Place.where(city: @city).where(type_of_site: type).where(lat: range_lat).where(lon: range_lon)
 	end
 
+	def generate_random_point range_lat, range_lon
+		type = get_type_random
+		Place.where(city: @city).where(type_of_site: type).where(lat: range_lat).where(lon: range_lon)
+	end
+
 	def get_type_begining
 		if @preferences[:tourist].to_i >= 2
-			x = ["Monument", "Museum", "Other"] 
+			x = @cultural_types
 		elsif @preferences[:relax].to_i >= 2 
-			x = ["Park", "Cafe", "Beautiful Place", "Beach"]
+			x = @relax_types
 		elsif @preferences[:drink].to_i >= 3 
-			x = ["Bar", "Disco", "Party"]
+			x = @drink_types
 		else
 			x = ["Monument", "Park", "Other", "Beautiful Place"] 
 		end
 		x
+	end
+
+	def get_type_random
+		# get number of type of sites planned of each
+		tourist,relax,drink = 0,0,0
+		@plan.map do |item|
+			tourist += 1 if @cultural_types.include? item.type_of_site
+			relax += 1 if @relax_types.include? item.type_of_site
+			drink += 1 if @drink_types.include? item.type_of_site
+		end
+		#new sizes to choose
+		tourist = @preferences[:tourist].to_f - (tourist * Random.rand)
+		relax = @preferences[:relax].to_f - (relax * Random.rand)
+		drink = @preferences[:drink].to_f - (drink * Random.rand)
+		x = Random.rand((tourist + relax + drink).abs)
+
+		if x <= tourist
+			return @cultural_types
+		elsif x <= tourist + relax 
+			return @relax_types
+		else
+			return @drink_types
+		end
 	end
 end
