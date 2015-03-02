1f56020a5db7ef04ba621c0c71934bb858461214
Mingos <dcividanes91@gmail.com>
2015-03-02 01:05:20 +0100

    styles to welcome page

--- app/assets/javascripts/register.js
+++ app/assets/javascripts/register.js
@@ -1,5 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('click','.toggleLogin',function(){
-	$("#login-register").toggleClass("js-fadeInRight--active");
+	$("#login-form, #login-form-social").toggleClass("js-fadeInRight--active");
+	$(".intro").toggle();
+	return false;
 });
