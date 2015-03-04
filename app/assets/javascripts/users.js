65053f14bbeaf56f9ef229470374823c16981f35
Mingos <dcividanes91@gmail.com>
2015-03-04 17:18:19 +0100

    v 1.0.demo

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,3 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-//edit user form
-$(document).ready(function(){
-	$('input[type="file"]').change(function(){
-		$(this).siblings("label").addClass("success-upload");
-	})
-});
--- app/controllers/suggests_controller.rb
+++ app/controllers/suggests_controller.rb
@@ -5,6 +5,7 @@ class SuggestsController < ApplicationController
 	def search
 		suggest = Suggester.new(params)
 		@plan = suggest.result
+		@plan = suggest.result if @plan.empty? #reduce the probabilities to have empty page
 		if @plan.empty?
 			render 'no_place'
 		else
