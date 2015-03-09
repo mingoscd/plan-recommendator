6dfda531aff3cb0c3d4d1e1d2b16418da5e53c54
Mingos <dcividanes91@gmail.com>
2015-03-09 20:41:33 +0100

    delete presentation changes

--- app/assets/javascripts/suggest_form.js
+++ app/assets/javascripts/suggest_form.js
@@ -25,26 +25,3 @@ function resizeBg(){
 	$(".suggest-content").css('height', hei+'px').css('min-height', min_hei+'px');
 	$(".suggest-index-bg").css('height', hei+'px').css('min-height', min_hei+'px');		
 }
-// function textTourist(element){
-// 	var description = element.siblings(".description");
-// 	switch(element.val()){
-// 		case "0": description.html("I already visited the city"); break;
-// 		case "1": break;
-// 		case "2": break;
-// 		case "3": description.html('Let\'s do a tour around the city'); break;
-// 		case "4": description.html('Let\'s do a tour around the city'); break;
-// 		case "5": description.html('Visit all the places! <img src="http://troll.me/images/x-all-the-things/lets-do-this.jpg" style="display: inline; height: 70px; width: 100px; vertical-align: middle;">'); break;
-// 	}
-// }
-
-// function textMoney(element){
-// 	var description = element.siblings(".description");
-// 	switch(element.val()){
-// 		case "0": description.html("I don't need money to have fun"); break;
-// 		case "1": break;
-// 		case "2": break;
-// 		case "3": description.html('I don\'t care about the money'); break;
-// 		case "4": break;
-// 		case "5": description.html('<img src="http://genophoria.files.wordpress.com/2012/11/shut-up-and-take-my-money.jpg" style="display: inline; height: 70px; width: 100px; vertical-align: middle;">'); break;
-// 	}
-// }
